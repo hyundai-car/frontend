c61cefc724650b668c69b7b125b9373b504bc278
jinii9 <jinii915@naver.com>
2025-01-12 12:46:26 +0900

    setup: global.style.ts

--- web/src/app/styles/global.style.ts
+++ web/src/app/styles/global.style.ts
@@ -11,9 +11,29 @@ const GlobalStyle = createGlobalStyle`
     }
 	
 	:root {
-		/* colors */
+		/* Color styles */
+		--navy: rgba(15, 23, 42, 1);
+		--blue: rgba(86, 98, 246, 1);
+		--gray-blue: rgba(203, 213, 225, 1);
+		--light-gray: rgba(247, 247, 247, 1);
+		--dark-gray: rgba(169, 169, 169, 1);
 
-		/* fonts */
+		/* Text-size styles */
+		--lg-small: 20px;
+		--semi-bold--xs: 9px;
+		--semi-bold--sm: 9px;
+		--semi-bold--md: 14px;
+		--semi-bold--md-small: 17px;
+		--semi-bold--lg: 24px;
+		--semi-bold--xxl: 32px;
+		--regular--xs: 7px;
+		--regular--md-small: 17px;
+		--regular--md: 14px;
+		--regular--sm: 9px;
+
+		/* Effect styles */
+		--list-item:  0px 4px 35px rgba(0, 0, 0, 0.15);
+
 	}
 `;
 
