2145399fd2065a276d5711925bdeb50c70703318
jinii9 <jinii915@naver.com>
2025-01-14 00:35:35 +0900

    markup: BasicButton (#5)

---
+++ web/src/shared/ui/button/BasicButton.ui.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { DefaultTheme } from "styled-components";
+
+interface BasicButtonProps {
+  onClick: () => void;
+  color?: keyof DefaultTheme["colors"];
+  disabled: boolean;
+  children: React.ReactNode;
+}
+
+export const BasicButton = ({
+  onClick,
+  children,
+  color = "blue",
+  disabled = false,
+}: BasicButtonProps) => {
+  return (
+    <StyledButton $color={color} disabled={disabled} onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<{
+  $color: keyof DefaultTheme["colors"];
+}>`
+  width: 100%;
+  height: 60px;
+  color: var(--white);
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.colors[props.$color]};
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  border: none;
+  outline: none;
+`;
