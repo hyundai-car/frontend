ee76027bede262929cacd46e465fd5957c10be39
hyejeong <67236054+softourr@users.noreply.github.com>
2025-01-14 03:20:53 +0900

    feat : search ing

--- web/src/pages/search/ui/search.ui.tsx
+++ web/src/pages/search/ui/search.ui.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
+
 export function SearchPage() {
   return (
     <div>
       <h1>SearchPage</h1>
+      <Input 
+  placeholder="이름을 입력하세요"
+/>
+
+      <Button color="navy" onClick={function (): void {
+        console.log("clicked");
+      }} disabled={false}>차알못을 위한 간편 진단</Button>
     </div>
   );
 }
