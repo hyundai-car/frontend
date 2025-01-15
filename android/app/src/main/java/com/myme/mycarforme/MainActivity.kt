a81a3975464dc28766317fee63db6c0e3a3ec04c
sejin jeong <sejin8603@naver.com>
2025-01-15 12:30:14 +0900

    feat: 안드로이드 검색 화면 웹뷰 로딩 성능 개선 (#4)

--- android/app/src/main/java/com/myme/mycarforme/MainActivity.kt
+++ android/app/src/main/java/com/myme/mycarforme/MainActivity.kt
@@ -2,21 +2,27 @@ package com.myme.mycarforme
 
 import android.os.Bundle
 import android.view.View
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsCompat.CONSUMED
 import androidx.core.view.updatePadding
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.myme.mycarforme.databinding.ActivityMainBinding
+import com.myme.mycarforme.ui.home.HomeFragment
+import com.myme.mycarforme.ui.my.Myfragment
+import com.myme.mycarforme.ui.recommend.RecommendFragment
+import com.myme.mycarforme.ui.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var activeFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +31,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val recommendFragment = RecommendFragment()
+        val myFragment = Myfragment()
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_recommend, R.id.navigation_my
-            )
-        )
+
         navView.setupWithNavController(navController)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.nav_host_fragment_activity_main, homeFragment, "FIRST").hide(homeFragment)
+            .add(R.id.nav_host_fragment_activity_main, searchFragment, "SECOND").hide(searchFragment)
+            .add(R.id.nav_host_fragment_activity_main, recommendFragment, "THIRD").hide(recommendFragment)
+            .add(R.id.nav_host_fragment_activity_main, myFragment, "FOURTH").hide(myFragment)
+            .commit()
+        activeFragment = homeFragment
+        supportFragmentManager.executePendingTransactions()
+        supportFragmentManager.beginTransaction().show(homeFragment).commit()
+        binding.navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> switchFragment(homeFragment)
+                R.id.navigation_search -> switchFragment(searchFragment)
+                R.id.navigation_recommend -> switchFragment(recommendFragment)
+                R.id.navigation_my -> switchFragment(myFragment)
+            }
+            true
+        }
         ViewCompat.setOnApplyWindowInsetsListener(
             window.decorView
         ) { v, insets ->
@@ -44,4 +66,11 @@ class MainActivity : AppCompatActivity() {
             return@setOnApplyWindowInsetsListener CONSUMED
         }
     }
+    private fun switchFragment(fragment: Fragment) {
+        if (activeFragment != fragment) {
+            supportFragmentManager.beginTransaction().hide(activeFragment!!).show(fragment).commit()
+            activeFragment = fragment
+        }
+    }
+
 }
--- android/app/src/main/java/com/myme/mycarforme/ui/search/SearchFragment.kt
+++ android/app/src/main/java/com/myme/mycarforme/ui/search/SearchFragment.kt
@@ -1,6 +1,8 @@
 package com.myme.mycarforme.ui.search
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +20,7 @@ class SearchFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    val binding get() = _binding!!
     private lateinit var webView: WebView
 
     override fun onCreateView(
@@ -28,22 +30,24 @@ class SearchFragment : Fragment() {
     ): View {
         val searchViewModel =
             ViewModelProvider(this).get(SearchViewModel::class.java)
-
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
         webView = binding.searchWebview
-
-
         setupWebView()
-        webView.loadUrl("http://192.168.201.101:3000/search")
-
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
         return root
     }
-    private fun setupWebView() {
-        webView.webViewClient = WebViewClient() // 내부 WebView에서 열리도록 설정
-        val webSettings: WebSettings = webView.settings
-        webSettings.javaScriptEnabled = true // JavaScript 사용 가능하게 설정
-        webSettings.domStorageEnabled = true // DOM 저장소 활성화
+
+
+    fun setupWebView() {
+        webView?.webViewClient = WebViewClient() // 내부 WebView에서 열리도록 설정
+        val webSettings: WebSettings? = webView?.settings
+        if (webSettings != null) {
+            webSettings.javaScriptEnabled = true
+            webSettings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+            webSettings.domStorageEnabled = true // DOM 저장소 활성화
+        }
+        webView?.loadUrl("http://192.168.201.101:3000/search")
     }
 
 
