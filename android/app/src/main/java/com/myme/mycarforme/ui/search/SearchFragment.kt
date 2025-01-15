cb5046624f418a5defa624d2c5c9648d1b0d4603
sejin jeong <sejin8603@naver.com>
2025-01-15 10:12:57 +0900

    feat: 안드로이드 검색 화면 웹뷰 연결 중 (#4)

--- android/app/src/main/java/com/myme/mycarforme/ui/search/SearchFragment.kt
+++ android/app/src/main/java/com/myme/mycarforme/ui/search/SearchFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +19,7 @@ class SearchFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var webView: WebView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,13 +31,21 @@ class SearchFragment : Fragment() {
 
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        webView = binding.searchWebview
 
-        val textView: TextView = binding.textNotifications
-        searchViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+        setupWebView()
+        webView.loadUrl("http://192.168.201.101:3000/search")
+
         return root
     }
+    private fun setupWebView() {
+        webView.webViewClient = WebViewClient() // 내부 WebView에서 열리도록 설정
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true // JavaScript 사용 가능하게 설정
+        webSettings.domStorageEnabled = true // DOM 저장소 활성화
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
