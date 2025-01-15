0fa6642ff7bd0680dfabe8ea718dee6bb2492aa6
jinii9 <jinii915@naver.com>
2025-01-15 17:16:46 +0900

    markup: 그래프 (#1)

--- web/src/entities/recommendation/ui/Graph.ui.tsx
+++ web/src/entities/recommendation/ui/Graph.ui.tsx
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
+import {
+  graphData,
+  graphOptions,
+} from "@/entities/recommendation/model/constants";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
 export function Graph() {
-  return <></>;
+  return (
+    <Container>
+      <Radar data={graphData} options={graphOptions} />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding: 10px 0 35px;
+`;
