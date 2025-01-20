6423d65e4cc893bbeaf7db75b157b5526ba81710
sejin jeong <sejin8603@naver.com>
2025-01-20 14:55:39 +0900

    feat: 정해진 url 벗어나면 헤더, 바텀 네비게이션 바 사라지게 하기 완료 #34

--- android/app/src/main/java/com/myme/mycarforme/ui/recommend/RecommendFragment.kt
+++ android/app/src/main/java/com/myme/mycarforme/ui/recommend/RecommendFragment.kt
@@ -1,7 +1,10 @@
 package com.myme.mycarforme.ui.recommend
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.JavascriptInterface
@@ -12,6 +15,7 @@ import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.myme.mycarforme.R
 import com.myme.mycarforme.WebAppInterface
 import com.myme.mycarforme.data.utils.SharedPrefs
 import com.myme.mycarforme.databinding.FragmentRecommendBinding
@@ -24,20 +28,24 @@ class RecommendFragment : Fragment() {
     // onDestroyView.
     private lateinit var webView: WebView
     private val binding get() = _binding!!
+    private lateinit var toolbar: View
+    private lateinit var bottomNavigation: View
+    private val basePath = "/recommendation/candidates"
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val recommendViewModel =
-            ViewModelProvider(this).get(RecommendViewModel::class.java)
 
         _binding = FragmentRecommendBinding.inflate(inflater, container, false)
         val root: View = binding.root
         webView = binding.recommendWebview
         setupWebView()
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+        toolbar = requireActivity().findViewById(R.id.main_toolbar)
+        bottomNavigation = requireActivity().findViewById(R.id.nav_view)
+        setupWebViewTouchListener()
         return root
     }
 
@@ -53,6 +61,40 @@ class RecommendFragment : Fragment() {
         }
         webView?.loadUrl("http://mycarf0r.me/recommendation/candidates")
         webView.addJavascriptInterface(WebAppInterface(requireContext()), "AndroidBridge")
+    }
+
+    private fun setupWebViewTouchListener() {
+        webView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_UP) {
+
+                webView.postDelayed({
+                    val currentUrl = webView.url
+                    Log.d("WebView Touch", "Current URL: $currentUrl")
+
+                    if (currentUrl != null && isSamePath(currentUrl)) {
+                        showUI()
+                    } else {
+                        hideUI()
+                    }
+                }, 100)
+            }
+            false // 터치 이벤트를 WebView 자체에서도 처리하도록 false 반환
+        }
+    }
+    private fun isSamePath(url: String): Boolean {
+        // URL의 path 추출
+        val uri = Uri.parse(url)
+        return uri.path == basePath
+    }
+
+    private fun hideUI() {
+        toolbar.visibility = View.GONE
+        bottomNavigation.visibility = View.GONE
+    }
+
+    private fun showUI() {
+        toolbar.visibility = View.VISIBLE
+        bottomNavigation.visibility = View.VISIBLE
     }
 
 
--- android/app/src/main/java/com/myme/mycarforme/ui/search/SearchFragment.kt
+++ android/app/src/main/java/com/myme/mycarforme/ui/search/SearchFragment.kt
@@ -1,18 +1,22 @@
 package com.myme.mycarforme.ui.search
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.myme.mycarforme.R
 import com.myme.mycarforme.WebAppInterface
 import com.myme.mycarforme.data.utils.SharedPrefs
 import com.myme.mycarforme.databinding.FragmentSearchBinding
@@ -26,6 +30,9 @@ class SearchFragment : Fragment() {
     // onDestroyView.
     val binding get() = _binding!!
     private lateinit var webView: WebView
+    private lateinit var toolbar: View
+    private lateinit var bottomNavigation: View
+    private val basePath = "/search"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,7 +45,12 @@ class SearchFragment : Fragment() {
         val root: View = binding.root
         webView = binding.searchWebview
         setupWebView()
+        toolbar = requireActivity().findViewById(R.id.main_toolbar)
+        bottomNavigation = requireActivity().findViewById(R.id.nav_view)
+
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+        setupWebViewTouchListener()
         return root
     }
 
@@ -46,16 +58,77 @@ class SearchFragment : Fragment() {
         webView.webViewClient = WebViewClient() // 내부 WebView에서 열리도록 설정
         webView.webChromeClient = WebChromeClient()
         webView.clearCache(true)
-        val webSettings: WebSettings? = webView?.settings
+        val webSettings: WebSettings = webView.settings
         if (webSettings != null) {
             webSettings.javaScriptEnabled = true
             //TODO: cachemode 바꾸기
             webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
             webSettings.domStorageEnabled = true // DOM 저장소 활성화
         }
-        webView.loadUrl("http://mycarf0r.me/search")
-        webView.addJavascriptInterface(WebAppInterface(requireContext()), "AndroidBridge")
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                Log.d("chk","$url")
+                // URL 변경에 따라 UI를 숨기거나 보이기
+                if (url != null && isSamePath(url)) {
+                    Log.d("chk","$url")
+                    showUI()
+                } else {
+                    hideUI()
+                }
+            }
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val url = request?.url.toString()
+                Log.d("URL Override", url)
 
+                // URL 변경 시 동일한 경로인지 확인
+                if (url.isNotEmpty() && isSamePath(url)) {
+                    showUI()
+                } else {
+                    hideUI()
+                }
+
+                // WebView가 자체적으로 URL을 로드하도록 false 반환
+                return false
+            }
+        }
+        webView.loadUrl("https://mycarf0r.me$basePath")
+        webView.addJavascriptInterface(WebAppInterface(requireContext()), "AndroidBridge")
+    }
+
+    private fun setupWebViewTouchListener() {
+        webView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_UP) {
+
+                webView.postDelayed({
+                    val currentUrl = webView.url
+                    Log.d("WebView Touch", "Current URL: $currentUrl")
+
+                    if (currentUrl != null && isSamePath(currentUrl)) {
+                        showUI()
+                    } else {
+                        hideUI()
+                    }
+                }, 100)
+            }
+            false // 터치 이벤트를 WebView 자체에서도 처리하도록 false 반환
+        }
+    }
+
+    private fun isSamePath(url: String): Boolean {
+        // URL의 path 추출
+        val uri = Uri.parse(url)
+        return uri.path == basePath
+    }
+
+    private fun hideUI() {
+        toolbar.visibility = View.GONE
+        bottomNavigation.visibility = View.GONE
+    }
+
+    private fun showUI() {
+        toolbar.visibility = View.VISIBLE
+        bottomNavigation.visibility = View.VISIBLE
     }
 
 
